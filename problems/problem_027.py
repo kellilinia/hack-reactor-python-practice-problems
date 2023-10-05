87c1cede1001079857d91863970a73f9c353e172
Kelly Khalilinia <kellykhalilinia@gmail.com>
2023-10-04 18:36:27 -0700

    Problem 27 Solution

--- problems/problem_027.py
+++ problems/problem_027.py
@@ -5,4 +5,10 @@
 #
 
 def max_in_list(values):
-    pass
+    if len(values) == 0:
+        return None
+    max_value = values[0]
+    for value in values:
+        if value > max_value:
+            max_value = value
+    return max_value
